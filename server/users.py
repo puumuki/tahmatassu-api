4abf07c8b462557133a78a7b396c4dfb4a1e000b
Teemu Puukko <teemuki@gmail.com>
2014-04-05 21:30:03 +0300

    'User.py'

--- server/users.py
+++ server/users.py
@@ -1,2 +1,2 @@
-users = {'teemu':{'hash':'00f6168fc33b3e08f7804ed4c790f31b9a6a0afb'},
-	   	 'ressu':{'hash':'00f6168fc33b3e08f7804ed4c790f31b9a6a0afb'}}
+users = {'test':{'hash':'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'}}
+	   	 
