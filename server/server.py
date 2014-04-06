e34db5c7a36810c51dc1e51d3e945faf13156449
Teemu Puukko <teemuki@gmail.com>
2014-04-06 09:57:45 +0300

    Add no cache-control

--- server/server.py
+++ server/server.py
@@ -182,6 +182,16 @@ def put_recipe():
 def list_recipes():
 	return json.dumps(storage.list())
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 @app.errorhandler(404)
 def page_not_found(error):
 	app.logger.error(error)	
