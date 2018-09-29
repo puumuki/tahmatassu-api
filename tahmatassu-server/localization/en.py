1e7b5babc00a24e702a08783eb5648822e50306d
Teemu Puukko <teemuki@gmail.com>
2018-09-29 23:39:49 +0300

    Fixed localisations

--- tahmatassu-server/localization/en.py
+++ tahmatassu-server/localization/en.py
@@ -5,11 +5,15 @@ keys = {
 	'recipe.was.saved.succesfully':'Recipe was saved succesfully',
 	'recipe.was.save.failed.name.error':'Recipe saving failed, the given filename is not accepted',
 	'recipe.was.save.failed.text.too.sort.error':'Saving the recipe failed, the content was too sort, it has to be least one charapter long.',
+	'recipe.delete': u'Recipe is marked delted',
+	'recipe.not_deleted': u'Recipe was not found with given name',
 	'missing.key':'Missing key %s',
 	'request.is.expecting.json':'Request is expecting to get JSON-data',
 	'resource.was.not.found':'Resource was not found',	
 	'login.granted' : u'You have logged in',
 	'login.denied' : u'Login failed, please try again',
 	'logout.success' : u'You have logged out',
-	'ask.recipe.howto': u'Search recipe from here, with a name: /recipe/:name'
+	'ask.recipe.howto': u'Search recipe from here, with a name: /recipe/:name',
+	'authenticated': u'Signed in',
+	'access.denied': u'Access denied'
 }
