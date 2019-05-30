0382126bb0d571cc60467e1f86de2cc15417fb13
Teemu Puukko <teemuki@gmail.com>
2019-05-30 12:39:29 +0300

    Users and server configs to vesrion management

---
+++ tahmatassu-server/server_config.py
@@ -0,0 +1,35 @@
+import os
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+#Secret key for Flask Cookies
+SECRET_KEY =  os.environ.get('SECRET_KEY', None)
+LANGUAGE = 'fi'
+HOST = '0.0.0.0'
+PORT = 9001
+
+#Directory where recipes are stored
+RECIPE_DIRECTORY = os.path.join( BASE_PATH, 'recipes')
+
+#File where user password hashes and privileges are stored
+USER_STORAGE = os.path.join( BASE_PATH, 'users.json')
+
+#URL used to reference to image files
+BASE_URL = "http://localhost:%s/static/img" % ( PORT, )
+
+#Directory where images and other stuff can be loaded, not implementet touch..
+UPLOAD_DIRECTORY = os.path.join( BASE_PATH, 'static', 'img')
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+MAX_CONTENT_LENGTH = 1024 * 1024
+
+#Directory where recipes are stored
+RECIPE_DIRECTORY = os.path.join( BASE_PATH, 'recipes')
+
+#Automatically reload engine on fatal error?
+ENGINE_AUTO_RELOAD = True
+
+#Log Server Output to Screen
+LOG_SCREEN = True
