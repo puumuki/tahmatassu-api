873bf89af084077798b95bf6f59046e75296bcb9
Teemu Puukko <teemuki@gmail.com>
2019-05-30 13:06:59 +0300

    Herokutest

--- tahmatassu-server/server.py
+++ tahmatassu-server/server.py
@@ -327,10 +327,7 @@ def upload_error(error):
   return redirect('/upload?error=true')
 
 def server(environ, start_response):
-  app.run(host=app.config.get('HOST', '0.0.0.0'),
-          port=app.config.get('PORT', ),
-          debug=app.config.get('DEBUG', False))
-  return "OK"
+  app.run()
 
 if __name__ == "__main__":
   app.run(host=app.config.get('HOST', '0.0.0.0'),
